7929e6dc931726c3bbf396417d6138a99523194b
ViniDevBR <vinigtr386@gmail.com>
2022-11-20 17:02:02 -0300

    :fire: Page Login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,63 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Button } from "../../components/Button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Container, LoginContainer, Column, Spacing, Title, InputContainer, ErrorMessage } from "./styles";
+
+export interface IFormLogin {
+  email: string;
+  password: string;
+}
+
+const defaultValues: IFormLogin = {
+  email: '',
+  password: ''
+}
+
+const schema = yup.object({
+  email: yup.string().email('Email invalido').required('Campo Obrigatório'),
+  password: yup.string().min(6, 'Não esta faltando alguma coisa?').required('Campo Obrigatório'),
+})
+
+export function Login() {
+
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm<IFormLogin>({
+    defaultValues: defaultValues,
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+    reValidateMode: 'onChange'
+  });
+
+  const onSubmit: SubmitHandler<IFormLogin> = data => console.log(data);
+
+  return(
+    <Container>
+      <LoginContainer onSubmit={handleSubmit(onSubmit)}>
+        <Column>
+          <Title>Login</Title>
+          <Spacing />
+          <InputContainer>
+            <input
+              type='text'
+              placeholder="Email"
+              {...register("email", { required: true })}
+            />
+          </InputContainer>
+          {errors.email && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
+          <Spacing />
+          
+          <InputContainer>
+            <input
+              type="password"
+              placeholder="Senha"
+              {...register("password", { required: true })}
+              />
+          </InputContainer>
+          {errors.password && <ErrorMessage>{errors.password?.message}</ErrorMessage>}
+          <Spacing />
+          <Button title="Entrar" disabled={!isValid}/>
+        </Column>
+      </LoginContainer>
+    </Container>
+  )
+}
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%; 
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoginContainer = styled.form`
+  width: 100%;
+  max-width: 420px;
+  background-color: #FFFFFF;
+  border-radius: 8px;
+  padding: 20px;
+  flex:1;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`
+
+export const Title = styled.h1`
+  font-size: 42px;
+  font-weight: 700;
+  color: #81259D;
+`
+
+export const Row = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`
+
+export const Column = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 42px;
+  color: #FFF;
+  border: 1px solid #81259D;
+  border-radius: 21px;
+  overflow: hidden;
+  padding: 0 10px;
+  
+  input {
+    width: 100%;
+    height: 42px;
+    border-radius: 21px;
+    background-color: transparent;
+    border: 0;
+    outline: none;
+  }
+`
+
+export const Spacing = styled.div`
+  margin: 10px 0;
+`
+
+export const ErrorMessage = styled.p`
+  color: red;
+  font-size:12px;
+  margin-top:8px;
+  margin-left:10px;
+`
